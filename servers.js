d157bd956d559434ca912eedafc1850b8728c6b8
BaseData64 <heavitasfont@gmail.com>
2026-08-21 16:07:31 -0600

    Add files via upload

--- servers.js
+++ servers.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Esto le enseña a tu servidor a leer datos en formato JSON
+
 app.use(express.json());
 
-// Una "base de datos" temporal en la memoria RAM para guardar posts de prueba
+
 let publicaciones = [];
 
-// Ruta 1: Ver si el servidor responde (GET)
+
 app.get('/', (req, res) => {
     res.send('¡Hola! El servidor backend de Miiverse está activo.');
 });
 
-// Ruta 2: Recibir una publicación nueva (POST)
+
 app.post('/api/posts', (req, res) => {
     const textoDelPost = req.body.contenido;
 
@@ -21,7 +21,7 @@ app.post('/api/posts', (req, res) => {
         return res.status(400).json({ error: 'El post viene vacío' });
     }
 
-    // Guardamos el post en nuestra lista en memoria
+    
     const nuevo = { id: publicaciones.length + 1, contenido: textoDelPost, fecha: new Date() };
     publicaciones.unshift(nuevo);
 
@@ -30,12 +30,11 @@ app.post('/api/posts', (req, res) => {
     res.json({ success: true, guardado: nuevo });
 });
 
-// Ruta 3: Ver todos los posts guardados (GET)
+
 app.get('/api/posts', (req, res) => {
     res.json(publicaciones);
 });
 
-// Encender el servidor en el puerto 3000
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
